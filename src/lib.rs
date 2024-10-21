24f2a8392e23cfd6289b7b8aea44f1b8a488785b
helloburp <thebosscda@gmail.com>
2024-10-21 04:24:18 -0500

    Simplified input; made output valid

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,43 @@
-use std::fs;
-use serde::Deserialize;
+use std::fs; use serde::Deserialize;
 use serde_json;
 use std::error::Error;
 use std::fmt::Display;
 use image::{self, ImageBuffer};
-use palette::{color_difference::EuclideanDistance, FromColor, IntoColor, Lab, Srgb, Hsv};
+use palette::{
+    FromColor, Hsv,
+    IntoColor, Lab, Mix,
+    color_difference::EuclideanDistance,
+};
 
 #[derive(Deserialize)]
-pub struct Color {
-    r: u8, g: u8, b: u8
+pub struct MaskConfig {
+    keep_hue: bool,
+    keep_saturation: bool,
+    keep_value: bool,
 }
 
 
 #[derive(Deserialize)]
 pub struct Config {
-    distance_fade: Color,
-    keep_details: bool,
-    invulnerability_range_high: Color,
-    invulnerability_range_low: Color,
-    radiation_suit: Color,
-    item_pickup: Color,
+    distance_fade: palette::Srgb<u8>,
+    masking: MaskConfig,
+    // invulnerability_range_high: palette::Srgb<u8>,
+    // invulnerability_range_low: palette::Srgb<u8>,
+    // radiation_suit: palette::Srgb<u8>,
+    // item_pickup: palette::Srgb<u8>,
 }
 
 
-struct ColorIterator<'a> {
-    bytes: &'a [u8],
-    offset: usize
-}
-
-
+struct ColorIterator<'a> { bytes: &'a [u8], offset: usize }
 impl<'a> Iterator for ColorIterator<'a> {
-    type Item = Srgb;
+    type Item = palette::Srgb;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.offset < self.bytes.len() - 2 {
-            let color = Srgb::new(
-                self.bytes[self.offset] as f32,
-                self.bytes[self.offset + 1] as f32,
-                self.bytes[self.offset + 2] as f32,
+            let color = palette::Srgb::new(
+                (self.bytes[self.offset] as f32)/255.0,
+                (self.bytes[self.offset + 1] as f32)/255.0,
+                (self.bytes[self.offset + 2] as f32)/255.0,
             );
 
             self.offset += 3;
@@ -49,12 +49,6 @@ impl<'a> Iterator for ColorIterator<'a> {
     }
 }
 
-
-impl Display for Color {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "(r:{}, g:{}, b:{})", self.r, self.g, self.b)
-    }
-}
 
 pub struct Input {
     file_path: String,
@@ -86,69 +80,108 @@ pub fn build_input(
 
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("Distance fade: {}", &config.distance_fade);
-    let palette_bytes = get_playpal_buffer()?;
+    println!(
+        "Distance fade: {}",
+        serde_json::to_string(&config.distance_fade)?
+    );
+    let playpal_bytes = fs::read("src/assets/PLAYPAL.pal")?;
     let colormap_bytes = fs::read("src/assets/COLORMAP.cmp")?;
-    new_palette_image(&palette_bytes)?;
-    new_colormap_image(&palette_bytes, &colormap_bytes, 0)?;
+
+    let playpal_image = draw_playpal(&playpal_bytes)?;
+    let color_image = draw_colormap(&playpal_bytes, &colormap_bytes, 0)?;
+
+    playpal_image.save("PLAYPAL.png")?;
+    color_image.save("COLORMAP.png")?;
+
+    let dark_colormap_image = draw_colormap(
+        &playpal_bytes, &build_colormap(
+            &playpal_bytes,
+            &get_invulnerability_page_from_colormap(&colormap_bytes),
+            &config
+        ), 0
+    )?;
+
+    dark_colormap_image.save("DARK_COLORMAP.png")?;
+
     Ok(())
 }
 
-pub fn get_color_distance(c1: Srgb, c2: Srgb) -> f32 {
-    let lab1 = Lab::from_color(c1);
-    let lab2 = Lab::from_color(c2);
 
-    lab1.distance(lab2)
+pub fn get_invulnerability_page_from_colormap<'a>(
+    colormap: &'a [u8]
+) -> &'a [u8] {
+    &colormap[32*256..33*256]
+}
+
+
+pub fn build_colormap(
+    playpal: &[u8], invulnerability_colormap_page: &[u8], config: &Config
+) -> Vec<u8> {
+    let mut colormap = vec![];
+    let playpal_first_page = &playpal[0..256*3];
+
+    for n in 0..=31 {
+        let darkness = n * 8 as u8;
+        let new_bytes = new_colormap_bytes_at_distance(
+            playpal_first_page, config.distance_fade.into_format(),
+            darkness, &config.masking
+        );
+        colormap.extend(&new_bytes);
+    }
+
+    colormap.extend(invulnerability_colormap_page);
+    colormap.extend(&[0u8; 256]);
+
+
+    colormap
 }
 
 
 pub fn best_fit_pixel_offset(
-    colormap: &Vec<u8>, color: Srgb
+    colormap: &[u8], color: palette::Srgb
 ) -> usize {
-    3 * (ColorIterator {bytes: colormap, offset: 0})
-        .map(|cmp_color| get_color_distance(color.clone(), cmp_color.clone()))
+    (ColorIterator {bytes: colormap, offset: 0})
+        .map(|cmp_color| {
+            Lab::from_color(color).distance(Lab::from_color(cmp_color))
+        })
         .enumerate()
-        .max_by(|(_, a), (_, b)| a.partial_cmp(b).expect("Result cannot be NaN.") )
+        .min_by(|(_, a), (_, b)| {
+            a.partial_cmp(b).expect("Result cannot be NaN.")
+        })
         .map(|(index, _)| index).expect("Invalid input indeces.")
 }
 
 
 
-pub fn get_playpal_buffer() -> Result<Vec<u8>, Box<dyn Error>> {
-    let bytes = fs::read("src/assets/PLAYPAL.pal")?;
-    if bytes.len()%3 != 0 {
-        return Err("PLAYPAL.pal is not correct filesize.".into())
-    }
-    if bytes.len() != 14 * 256 * 3 {
-        return Err("PLAYPAL.pal is not correct length.".into())
-    }
-
-    Ok(bytes)
-}
-
-
-pub fn map_at_distance(
-    colormap: &Vec<u8>, fade_color: Srgb, distance: u8, keep_details: bool
+pub fn new_colormap_bytes_at_distance(
+    playpal_first_page: &[u8],
+    fade_color: palette::Srgb,
+    distance: u8,
+    mask_config: &MaskConfig
 ) -> Vec<u8> {
-    let mix = (distance as f32)/255.0;
-
-    (ColorIterator {bytes: colormap, offset: 0})
+    (ColorIterator {bytes: playpal_first_page, offset: 0})
         .map(|cmp_color| {
-            let real_fade_color = match keep_details {
-                false => fade_color.clone(),
-                true => {
-                    let mut fade_hsv = Hsv::from_color(fade_color);
-                    let cmp_hsv = Hsv::from_color(cmp_color);
-                    fade_hsv.value = cmp_hsv.value;
+            let cmp_hsv = Hsv::from_color(cmp_color);
+            let mut fade_hsv = Hsv::from_color(fade_color);
+            fade_hsv.saturation =
+                (if mask_config.keep_saturation { cmp_hsv } else { fade_hsv }).saturation;
+            fade_hsv.hue =
+                (if mask_config.keep_hue { cmp_hsv } else { fade_hsv }).hue;
+            fade_hsv.value =
+                (if mask_config.keep_value { cmp_hsv } else { fade_hsv }).value;
 
-                    Hsv::into_color(fade_hsv)
-                }
-            }
+            cmp_hsv.mix(fade_hsv, (distance as f32)/255.0).into_color()
         })
+        .map(|mixed_color| {
+            best_fit_pixel_offset(playpal_first_page, mixed_color) as u8
+        })
+        .collect()
 }
 
 
-pub fn new_palette_image(palette_bytes: &Vec<u8>) -> Result<(), Box<dyn Error>> {
+pub fn draw_playpal(
+    palette_bytes: &[u8]
+) -> Result<ImageBuffer<image::Rgb<u8>, Vec<u8>>, Box<dyn Error>> {
     let (imgx, imgy) = (16, (palette_bytes.len()/(16*3)) as u32);
 
     let mut imgbuf = ImageBuffer::new(imgx, imgy);
@@ -163,17 +196,15 @@ pub fn new_palette_image(palette_bytes: &Vec<u8>) -> Result<(), Box<dyn Error>> 
         *pixel = image::Rgb([r,g,b]);
     }
 
-    imgbuf.save("PLAYPAL.png")?;
-
-    Ok(())
+    Ok(imgbuf)
 }
 
 
-pub fn new_colormap_image(
-    palette_bytes: &Vec<u8>,
-    colormap_bytes: &Vec<u8>,
+pub fn draw_colormap(
+    palette_bytes: &[u8],
+    colormap_bytes: &[u8],
     palette_select: u32
-) -> Result<(), Box<dyn Error>> {
+) -> Result<ImageBuffer<image::Rgb<u8>, Vec<u8>>, Box<dyn Error>> {
 
     let (imgx, imgy) = (16, (colormap_bytes.len()/16) as u32);
 
@@ -194,9 +225,7 @@ pub fn new_colormap_image(
         *pixel = image::Rgb([r,g,b]);
     }
 
-    imgbuf.save("COLORMAP.png")?;
-
-    Ok(())
+    Ok(imgbuf)
 }
 
 
